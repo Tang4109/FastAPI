892b6185267fdf1607e69449174f81939e52d1bb
Tang4109 <2298964704@qq.com>
2024-05-10 14:30:43 +0800

    主入口文件

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.staticfiles import StaticFiles
 from apps.Steel_surface_defect_detection.app import app_detection
+from apps.LLM.app import app_LLM
 
 app = FastAPI()
-
 # 静态文件
 app.mount("/static", StaticFiles(directory="statics"))
-
 app.include_router(app_detection, tags=["缺陷检测"])
+app.include_router(app_LLM, tags=["LLM"])
 
 
 if __name__ == '__main__':
